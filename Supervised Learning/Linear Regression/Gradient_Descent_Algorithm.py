3afd1b8d90890a40d548e19137afa6d0f6402020
Anthony D'souza <jordananthony786@gmail.com>
2022-01-25 19:27:41 +0530

    Update Gradient_Descent_Algorithm.py

--- Supervised Learning/Linear Regression/Gradient_Descent_Algorithm.py
+++ Supervised Learning/Linear Regression/Gradient_Descent_Algorithm.py
@@ -60,6 +60,7 @@ def coefficients(trainSet):
         X2_data.append(row[1])
         X3_data.append(row[2])
         Y_data.append(row[3]) 
+    print(f"Training:- Actual Set = {Y_data}")
     while((stepSize0>=0.001 or stepSize1>=0.001 or stepSize2>=0.001 or stepSize3>=0.001) and epoch<=max_iterations):
         yBar = prediction(trainSet, beta)
         stepSize0 = alpha * sum_of_derivative(Y_data, X0_data, yBar)
@@ -74,23 +75,52 @@ def coefficients(trainSet):
     return [beta0, beta1, beta2, beta3]
 
 
+# Set1 -> Set containing actual values and Set2 -> Set containing predicted values
+def root_mean_square_error(Set1, Set2): 
+    if len(Set1) == len(Set2):
+        totalError = 0.0 
+        iterations = len(Set1)
+        for iter in range(iterations):
+            predictionError = Set1[iter] - Set2[iter]
+            totalError = totalError + algebra.pow(predictionError, 2)
+        meanError = totalError/float(iterations)
+        return meanError
+    else:
+        return None
+        
+
 def testing(param):
     Size = float(input("Enter size of house(in feet^2) = "))
     Bedrooms = float(input("Enter no. of bedrooms = "))
     Age = float(input("Enter age of house(in years) = "))
-    yhat = param[0] + param[1]*Size + param[2]*Bedrooms + param[3]*Age
-    return yhat
+    House_Price = param[0] + param[1]*Size + param[2]*Bedrooms + param[3]*Age
+    return House_Price
 
 
+# Training the ML model using Gradient Descent Algorithm
+print("\n\t\t\t\t Training ML model \n")
 excel = 'House Price.csv'
 dataSet = load_CSV(excel)
 params = coefficients(dataSet)
 predictSet = prediction(params, dataSet)
-print(f"Predicted Set = \n\t{predictSet}") 
-print("\n\t\t\t\t House Price Prediction APP\n")
+print(f"Training:- Predicted Set = {predictSet}") 
+# Testing the ML model using Test Dataset
+print("\n\t\t\t\t Testing ML model \n")
+test_csv = 'House Price test.csv'
+test_dataSet = load_CSV(test_csv)
+actual_value = []
+for row in test_dataSet:
+    actual_value.append(row[-1])
+print(f"Testing:- Actual Set = {actual_value}")
+predicted_value = prediction(test_dataSet, params)
+print(f"Testing:- Predicted Set = {predicted_value}")
+RMSE = root_mean_square_error(actual_value, predicted_value)
+print(f"Root Mean Square Error = {RMSE}")
+# Deploying model to real world applications
+print("\n\n\t\t\t\t House Price Prediction APP\n")
 while True:
     result = testing(params)
-    print(f"Your house Price = {result}")
+    print(f"Your house Price = {result}") 
     Quit_status = input("\nDo you want to Quit?\t1.Y\t or \t2.No")
     if Quit_status.lower() == "yes" or Quit_status.lower() == 'y':
         break
